70b603cb84281b124f82cec5ef09f613a96112aa
Radifan Mardhi Prana <radif.vengeance@gmail.com>
2019-08-22 22:09:51 +0700

    index fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = proses.env.PORT || 4000
+const port = process.env.PORT || 4000
 
 
 app.use(bodyParser.json())
